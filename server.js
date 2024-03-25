bfc3d76a2b9bae9ff679ece07ee6458a3c680c8e
Markus Vickman~ <markus.vickman@hotmail.com>
2024-03-25 15:45:49 +0100

    lite styling av sidan

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: ""
+});
 
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const port = 3000;
 
+/*
+connection.connect((err) =>{
+    if(err){
+        console.error("Connection failed big!: " + err);
+        throw err;
+    }
+});*/
+
 app.set("view engine", "ejs");
 app.use(express.static("public")); //Statiska filer
+app.use(bodyParser.urlencoded({extended: true}));
 
 //route
 app.get("/", (req, res) => {
@@ -13,12 +30,16 @@ app.get("/", (req, res) => {
 });
 
 //route
-app.get("/", (req, res) => {
+app.get("/course", (req, res) => {
     res.render("course");
 });
 
+/*app.post("/", (req, res) => {
+    res.render("index");
+});*/
+
 //route
-app.get("/", (req, res) => {
+app.get("/about", (req, res) => {
     res.render("about");
 });
 
@@ -26,3 +47,4 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
     console.log("Express servern har startat på port:" + port);
 });
+
